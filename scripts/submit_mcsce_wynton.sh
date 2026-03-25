a95e5c6cd5ef3b31adf7acc219516e18073a3b32
Joyce Mo <joycemo413@gmail.com>
2026-03-25 12:26:16 -0700

    Update submit_mcsce_wynton.sh
    updated output path to scratch

--- scripts/submit_mcsce_wynton.sh
+++ scripts/submit_mcsce_wynton.sh
@@ -2,7 +2,7 @@
 #$ -S /bin/bash
 #$ -N mcsce
 #$ -cwd
-##$ -t 1-13129
+#$ -t 1-13129
 #$ -tc 200
 #$ -l h_rt=15:00:00
 #$ -l mem_free=48G
@@ -10,7 +10,8 @@
 #$ -o logs/mcsce_$JOB_ID.out
 #$ -e logs/mcsce_$JOB_ID.err
 #$ -r y
-#$ -m bea
+#$ -m be
+#$ -a 
 #$ -M joyce.mo@ucsf.edu
 #
 # MC-SCE ensemble generation on Wynton (SGE)
@@ -25,11 +26,11 @@
 export PYTHONPATH="/wynton/home/rotation/jqmo/rotation3/mcsce/src:${PYTHONPATH}"
 
 PDB_LIST="pdb_list.txt"
-OUTDIR="ensembles/mcsce"
+OUTDIR="/wynton/scratch/jqmo/rotation_datasets/ai-cath_mcsce_ensembles"
 NCONFS=5
 TEMPERATURE=300.0
 
-python run_mcsce.py \
+python scripts/run_mcsce.py \   
     --pdb_list "$PDB_LIST" \
     --task_id "$SGE_TASK_ID" \
     --outdir "$OUTDIR" \
