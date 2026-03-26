92570c219c43fc0c4b50f6bbadf5815250480d17
Joyce Mo <joycemo413@gmail.com>
2026-03-25 17:28:52 -0700

    Create submit_backrub_wynton.sh
    run pyrosetta on wynton cause savio queue is dummy

---
+++ scripts/submit_backrub_wynton.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+#$ -S /bin/bash
+#$ -N backrub
+#$ -cwd
+#$ -t 1-50
+#$ -tc 200
+#$ -l h_rt=02:00:00
+#$ -l mem_free=4G
+#$ -o logs/backrub_03252026_$JOB_ID.out
+#$ -e logs/backrub_03252026_$JOB_ID.err
+#
+
+source activate mcsce
+
+PDB_LIST="pdb_list_ai-cath_subset.txt"
+OUTDIR="/wynton/scratch/jqmo/rotation_datasets/ai-cath_backrub_subset_ensembles"
+NCONFS=5
+NSTEPS=10000
+KT=0.6
+
+python scripts/run_backrub.py \
+    --pdb_list "$PDB_LIST" \
+    --task_id "$SGE_TASK_ID" \
+    --outdir "$OUTDIR" \
+    --nconfs "$NCONFS" \
+    --nsteps "$NSTEPS" \
+    --kT "$KT" \
+    --mode pyrosetta
